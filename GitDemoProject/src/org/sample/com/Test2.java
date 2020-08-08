0000fc90d1bec51cf3666f156b7a5516e3f0532a
Navneeth Diwaker Naik <navneeth.naik@gmail.com>
2020-08-08 17:10:04 +0530

    Adding this to regression

--- GitDemoProject/src/org/sample/com/Test2.java
+++ GitDemoProject/src/org/sample/com/Test2.java
@@ -6,6 +6,7 @@ public class Test2 {
 		// TODO Auto-generated method stub
 			System.out.println("sample output");
 			System.out.println("sample output");
+			System.out.println("sample output");
 	}
 
 }
